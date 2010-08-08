f4529c90ef32a698145f1f39c39bc76ca332f793
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-08-08 14:14:35 +0200

    remove a few extraneous files and update gitignore.  Add some state transition tests to request transaction

--- test/unit/models/request_transaction_test.rb
+++ test/unit/models/request_transaction_test.rb
@@ -8,4 +8,40 @@ class RequestTransactionTest < ActiveSupport::TestCase
   test "test creating request transaction" do
     assert @request_transaction.id
   end
+  
+  test "create a req transaction and check initial state transition" do
+    assert_equal 'tentatively_due', @request_transaction.state
+  end
+  
+  test "take a report and mark_actually_due" do
+    @request_transaction.mark_actually_due
+    assert_equal 'actually_due', @request_transaction.state
+  end
+
+  test "take a report and mark_actually_due from tentatively_due" do
+    @request_transaction.state = 'tentatively_due'
+    @request_transaction.save
+    @request_transaction.mark_actually_due
+    assert_equal 'actually_due', @request_transaction.state
+  end
+
+  test "take a report and mark_paid" do
+    @request_transaction.mark_paid
+    assert_equal 'paid', @request_transaction.state
+  end
+  
+  test "take a report and mark paid from tentatively_due" do
+    @request_transaction.state = 'tentatively_due'
+    @request_transaction.save
+    @request_transaction.mark_paid
+    assert_equal 'paid', @request_transaction.state
+  end
+  
+  test "take a report and mark paid from mark_actually_due" do
+    @request_transaction.state = 'tentatively_due'
+    @request_transaction.save
+    @request_transaction.mark_paid
+    assert_equal 'paid', @request_transaction.state
+  end
+
 end
