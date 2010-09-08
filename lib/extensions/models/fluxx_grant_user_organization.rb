fedbcd2bdf845c40e48e10020ceca2e59f5650fa
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-09-08 14:04:21 -0700

    update user as well as org

--- lib/extensions/models/fluxx_grant_user_organization.rb
+++ lib/extensions/models/fluxx_grant_user_organization.rb
@@ -16,21 +16,21 @@ module FluxxGrantUserOrganization
 
   module ModelInstanceMethods
     def update_related_data
-      p "ESH: 111 in UserOrganization update_related_data"
       if User.respond_to? :indexed_by_sphinx?
-        p "ESH: 222 in UserOrganization update_related_data"
-        User.without_realtime do
-          p "ESH: 333 in UserOrganization update_related_data"
+        Organization.without_realtime do
           if organization_id
-            p "ESH: 444a in UserOrganization update_related_data"
             Organization.update_all 'delta = 1', ['id = ?', organization_id]
-            p "ESH: 444b in UserOrganization update_related_data"
             o = Organization.find(organization_id)
-            p "ESH: 444c in UserOrganization update_related_data"
             o.delta = 1
-            p "ESH: 444d in UserOrganization update_related_data"
             o.save 
-            p "ESH: 444e in UserOrganization update_related_data"
+          end
+        end
+        User.without_realtime do
+          if user_id
+            User.update_all 'delta = 1', ['id = ?', user_id]
+            o = User.find(user_id)
+            o.delta = 1
+            o.save 
           end
         end
       end
