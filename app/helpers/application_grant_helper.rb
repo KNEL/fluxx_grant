dfcd90224c4ed8af04d33f797738cea5778e98e9
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-09-09 09:40:52 -0700

    added a way to do req trans and req reports in HTML as attributes

--- app/helpers/application_grant_helper.rb
+++ app/helpers/application_grant_helper.rb
@@ -127,5 +127,41 @@ module ApplicationGrantHelper
     ((count == 1) ? singular : (plural || singular.pluralize))
   end
 
+  # This method demonstrates the use of the :child_index option to render a
+  # form partial for, for instance, client side addition of new nested
+  # records.
+  #
+  # This specific example creates a link which uses javascript to add a new
+  # form partial to the DOM.
+  #
+  #   <% form_for @project do |project_form| -%>
+  #     <div id="tasks">
+  #       <% project_form.fields_for :tasks do |task_form| %>
+  #         <%= render :partial => 'task', :locals => { :f => task_form } %>
+  #       <% end %>
+  #     </div>
+  #   <% end -%>
+  # Citation: http://github.com/alloy/complex-form-examples
+  def generate_html(form_builder, method, options = {})
+    options[:object] ||= form_builder.object.class.reflect_on_association(method).klass.new
+    options[:partial] ||= method.to_s.singularize
+    options[:form_builder_local] ||= :f  
+
+    form_builder.fields_for(method, options[:object], :child_index => 'NEW_RECORD') do |f|
+      render(:partial => options[:partial], :locals => { options[:form_builder_local] => f })
+    end
+  end
+
+  def generate_template(form_builder, method, options = {})
+    escape_carriage_returns(single_quote_html(generate_html(form_builder, method, options)))
+  end
+  
+  def single_quote_html html
+    html.gsub '"', "'"
+  end
+
+  def escape_carriage_returns html
+    html.gsub "\n", '\\n'
+  end
 
 end
