10319b9b86de2b7dea0a6f389621c0be4a7c1ab4
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-09-10 15:08:39 -0700

    make it so that choosing a satellite picks up people from the hq too

--- lib/extensions/controllers/fluxx_grant_users_controller.rb
+++ lib/extensions/controllers/fluxx_grant_users_controller.rb
@@ -4,6 +4,19 @@ module FluxxGrantUsersController
     base.insta_index User do |insta|
       insta.filter_title = "Users Filter"
       insta.filter_template = 'users/user_filter'
+      insta.pre do |controller_dsl, controller|
+        if controller.params[:related_organization_id]
+          rel_org_id = controller.params[:related_organization_id]
+          controller.pre_models = User.find_by_sql ['SELECT users.* FROM users, user_organizations 
+                                 WHERE user_organizations.organization_id IN 
+                                 (select distinct(id) from (select id from organizations where id = ? 
+                                  union select id from organizations where parent_org_id = ? 
+                                  union select id from organizations where parent_org_id = (select parent_org_id from organizations where id = ?) and parent_org_id is not null
+                                  union select parent_org_id from organizations where id = ?) all_orgs where id is not null) 
+                                 AND user_organizations.user_id = users.id', rel_org_id, rel_org_id, rel_org_id, rel_org_id]
+        end
+      end
+      
     end
     
     base.insta_related User do |insta|
