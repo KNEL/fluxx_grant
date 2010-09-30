5a2e3b91920942c289dc6614c962f9b34d57953f
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-09-30 15:25:30 -0700

    switch so that we automatically recompile SASS and javascript when requests come in for said resources

--- lib/fluxx_grant.rb
+++ lib/fluxx_grant.rb
@@ -33,21 +33,9 @@ Dir[File.dirname(__FILE__) + "/../app/helpers/**/*_helper.rb"].each do
   ActionController::Base.helper "#{File.basename(file,'.rb').camelize}".constantize
 end
 
-require 'rails/generators'
-
-class InternalFluxxEnginePublicGenerator < Rails::Generators::Base
-  include Rails::Generators::Actions
-
-  def self.source_root
-    File.join(File.dirname(__FILE__), 'templates')
-  end
-
-  def copy_fluxx_public_files
-    public_dir = File.join(File.dirname(__FILE__), '../public')
-    directory("#{public_dir}/images", 'public/images/fluxx_grant', :verbose => false)
-    directory("#{public_dir}/javascripts", 'public/javascripts/fluxx_grant', :verbose => false)
-    directory("#{public_dir}/stylesheets", 'public/stylesheets/fluxx_grant', :verbose => false)
-  end
-end
-
-InternalFluxxEnginePublicGenerator.new.copy_fluxx_public_files
+public_dir = File.join(File.dirname(__FILE__), '../public')
+DirectorySync.new [
+  ["#{public_dir}/images", '/images/fluxx_grant'],
+  ["#{public_dir}/javascripts", '/javascripts/fluxx_grant'],
+  ["#{public_dir}/stylesheets", '/stylesheets/fluxx_grant'],
+]
