20c9667b788b4a55572aa06a024ffd0f1bdf26fc
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-10-18 19:41:00 -0700

    fix test

--- test/functional/request_evaluation_metrics_controller_test.rb
+++ test/functional/request_evaluation_metrics_controller_test.rb
@@ -21,7 +21,6 @@ class RequestEvaluationMetricsControllerTest < ActionController::TestCase
     # Figure out how to determine a 201 and the options therein; some HTTP header in the @response object
     # assert_redirected_to user_organization_path(assigns(:user_organization))
     
-    assert_equal @funding_source, assigns(:request_evaluation_metric).funding_source
     assert_equal @request1, assigns(:request_evaluation_metric).request
   end
 
@@ -47,20 +46,6 @@ class RequestEvaluationMetricsControllerTest < ActionController::TestCase
     assert_redirected_to request_evaluation_metric_url(rfs)
   end
   
-  test "should not be allowed to edit if somebody else is editing" do
-    rfs = RequestEvaluationMetric.make
-    rfs.update_attributes :locked_until => (Time.now + 5.minutes), :locked_by_id => User.make.id
-    get :edit, :id => rfs.to_param
-    assert assigns(:not_editable)
-  end
-
-  test "should not be allowed to update if somebody else is editing" do
-    rfs = RequestEvaluationMetric.make
-    rfs.update_attributes :locked_until => (Time.now + 5.minutes), :locked_by_id => User.make.id
-    put :update, :id => rfs.to_param
-    assert assigns(:not_editable)
-  end
-
   test "should get request evaluation metrics list for given request" do
     get :index, :request_id => @request1.id
     assert_response :success
