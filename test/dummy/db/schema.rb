b1f8766b1d29ef5eaebc16e2531b0b02189efa5a
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-12-01 16:53:49 -0800

    FLX-243 change schema for tests for user_organizations

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101127174221) do
+ActiveRecord::Schema.define(:version => 20101202004553) do
 
   create_table "audits", :force => true do |t|
     t.datetime "created_at"
@@ -656,7 +656,6 @@ ActiveRecord::Schema.define(:version => 20101127174221) do
     t.string   "department",      :limit => 400
     t.string   "email",           :limit => 400
     t.string   "phone",           :limit => 400
-    t.datetime "deleted_at"
     t.datetime "locked_until"
     t.integer  "locked_by_id"
   end
