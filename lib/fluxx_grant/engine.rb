bbd5627babd8e829ae5e25f780d79b1221e65a34
Eric Hansen <ehansen486@gmail.com>
2010-12-23 11:39:36 -0800

    add in sphinx seeding

--- lib/fluxx_grant/engine.rb
+++ lib/fluxx_grant/engine.rb
@@ -5,6 +5,15 @@ module FluxxGrant
   class Engine < Rails::Engine
     initializer 'fluxx_engine.add_compass_hooks', :after=> :disable_dependency_loading do |app|
       Sass::Plugin.add_template_location "#{File.dirname(__FILE__).to_s}/../../app/stylesheets", "public/stylesheets/compiled/fluxx_grant"
+      # Make sure that sphinx indices are loaded properly
+      # In thinking sphinx's ThinkingSphinx::Context#add_indexed_models method, I ran rails console and then watched what order the classes are loaded
+      Organization rescue nil
+      RequestTransaction rescue nil
+      RequestReport rescue nil
+      Request rescue nil
+      User rescue nil
+      Project rescue nil
+      Request.sphinx_index_names rescue nil
     end
     rake_tasks do
       load File.expand_path('../../tasks.rb', __FILE__)
