a1ef7bb758b508f4d4c876787b83932ab022124b
Eric Hansen <ehansen486@gmail.com>
2010-12-23 14:03:27 -0800

    port back gem_handler

---
+++ lib/extensions/gem_handler.rb
@@ -0,0 +1,34 @@
+class GemHandler
+  def self.dependent_gems_block
+    (lambda do |params|
+      dev_local, cur_dir, gem_versions = params
+      gem 'thinking-sphinx', '2.0.1', :require => 'thinking_sphinx'
+      # gem "thinking-sphinx", :git => "https://github.com/freelancing-god/thinking-sphinx.git", :branch => "rails3", :require => 'thinking_sphinx'
+      if dev_local 
+        p "Installing dependent fluxx gems to point to local paths.  Be sure you install fluxx_engine, fluxx_crm and fluxx_grant in the same directory as the reference implementation."
+        if File.exist?("#{cur_dir}/../fluxx_engine")
+        	gem "fluxx_engine", gem_versions[:fluxx_engine], :path => "../fluxx_engine"
+        elsif File.exist?("#{cur_dir}/fluxx_engine")
+      	gem "fluxx_engine", gem_versions[:fluxx_engine], :path => "./fluxx_engine"
+        end
+
+        if File.exist?("#{cur_dir}/../fluxx_crm")
+        	gem "fluxx_crm", gem_versions[:fluxx_crm], :path => "../fluxx_crm"
+        elsif File.exist?("#{cur_dir}/fluxx_crm")
+      	gem "fluxx_crm", gem_versions[:fluxx_crm], :path => "./fluxx_crm"
+        end
+
+        if File.exist?("#{cur_dir}/../fluxx_grant")
+        	gem "fluxx_grant", gem_versions[:fluxx_grant], :path => "../fluxx_grant"
+        elsif File.exist?("#{cur_dir}/fluxx_grant")
+      	  gem "fluxx_grant", gem_versions[:fluxx_grant], :path => "./fluxx_grant"
+        end
+      else
+        p "Installing dependent fluxx gems."
+        gem "fluxx_engine", gem_versions[:fluxx_engine]
+        gem "fluxx_crm", gem_versions[:fluxx_crm], :require => 'fluxx_crm'
+        gem "fluxx_grant", gem_versions[:fluxx_grant], :require => 'fluxx_grant'
+      end
+    end)
+  end
+end
