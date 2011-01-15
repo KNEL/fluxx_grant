5b9cd3d9c0b6de4d2403b2bddbe2572ef5c873db
Eric Hansen <ehansen486@gmail.com>
2011-01-14 18:43:47 -0800

    switch thinking sphinx

--- lib/extensions/test/fluxx_grant_test_helper.rb
+++ lib/extensions/test/fluxx_grant_test_helper.rb
@@ -39,6 +39,48 @@ module FluxxGrantTestHelper
       end
     end
     "
+    
+    eval "
+    class ActionController::TestCase
+      #include Devise::TestHelpers
+    end
+
+    # Do not audit log during tests
+    module CollectiveIdea #:nodoc:
+      module Acts #:nodoc:
+        module Audited #:nodoc:
+          module InstanceMethods
+            private
+            def write_audit(attrs)
+              # Do nothing during tests
+              # self.audits.create attrs if auditing_enabled
+            end
+          end
+        end
+      end
+    end
+
+    # Swap out the thinking sphinx sphinx interface with actual SQL
+    module ThinkingSphinx
+      module SearchMethods
+        module ClassMethods
+
+          def search_for_ids(*args)
+            paged_objects = search *args
+            raw_ids = paged_objects.map &:id
+            WillPaginate::Collection.create paged_objects.current_page, paged_objects.per_page, paged_objects.total_pages do |pager|
+              pager.replace raw_ids
+            end
+          end
+
+          def search(*args)
+            self.paginate(:page => 1)
+          end
+        end
+      end
+    end
+    
+    "
   end
 
   module ModelClassMethods
