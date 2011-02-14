88512676e2dd7b9d8742776ef3acd96a055652d9
Eric Hansen <ehansen486@gmail.com>
2011-02-13 22:21:11 -0800

    fix the way we load the dependent gems

--- lib/extensions/gem_handler.rb
+++ lib/extensions/gem_handler.rb
@@ -8,18 +8,22 @@ class GemHandler
       # gem "thinking-sphinx", :git => "https://github.com/freelancing-god/thinking-sphinx.git", :branch => "rails3", :require => 'thinking_sphinx'
       if dev_local 
         p "Installing dependent fluxx gems to point to local paths.  Be sure you install fluxx_engine, fluxx_crm and fluxx_grant in the same directory as the reference implementation."
-        gem_versions.keys.each do |repo_name|
+        gem_versions.each do |repo_pair|
+          repo_name, version = repo_pair
           repo_name_string = repo_name.to_s
           
           if File.exist?("#{cur_dir}/../#{repo_name_string}")
-          	gem repo_name_string, gem_versions[repo_name], :path => "../#{repo_name_string}"
+          	gem repo_name_string, version, :path => "../#{repo_name_string}"
           elsif File.exist?("#{cur_dir}/#{repo_name_string}")
-        	  gem repo_name_string, gem_versions[repo_name], :path => "./#{repo_name_string}"
+        	  gem repo_name_string, version, :path => "./#{repo_name_string}"
           end
         end
       else
         p "Installing dependent fluxx gems."
-        gem_versions.keys.each {|repo_name| gem repo_name.to_s, gem_versions[repo_name]}
+        gem_versions.each do |repo_pair|
+          repo_name, version = repo_pair
+          gem repo_name.to_s, version
+        end
       end
     end)
   end
